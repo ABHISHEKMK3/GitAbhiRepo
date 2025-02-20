00a500c038a7f517ad80e0aaee03645fa0edc666
abhishek3.mk@gmail.com <abhishek3.mk@gmail.com>
2025-02-21 00:31:26 +0530

    This commit to come home

---
+++ src/test/java/com/gears42/mk/SeleniumInterview/TestGit.java
@@ -0,0 +1,17 @@
+package com.gears42.mk.SeleniumInterview;
+
+import org.testng.annotations.Test;
+
+public class TestGit {
+
+	
+	@Test
+	public void methodGee()
+	{
+		System.out.println("Hi Abhi");
+		System.out.println("This is Geetha Had Break Fast");
+		System.out.print("come Home");
+	}
+	
+	
+}
